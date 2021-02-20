ed9ed9bffeb36363e202657394b8b4ba824153ed
zawnk <zonkor@gmail.com>
2021-02-20 20:14:53 +0100

    Cleanup and add typing indicator

--- opencritic/helpers.py
+++ opencritic/helpers.py
@@ -1,6 +1,5 @@
 import logging
 import discord
-from datetime import datetime
 import aiohttp
 
 from .constants import API_URL
@@ -20,7 +19,6 @@ async def make_embed_from_gameid(gameId: int):
                 response.raise_for_status()
             game = await response.json()
 
-    log.debug('building embed for game '+game['name']+' '+str(datetime.now()))
     em = None
     description = None
     scoreInfo = None
--- opencritic/opencritic.py
+++ opencritic/opencritic.py
@@ -1,7 +1,6 @@
 import logging
 import aiohttp
 import asyncio
-from datetime import datetime
 from .constants import API_URL
 from .menus import BaseMenu, GameMenuPage
 
@@ -9,7 +8,7 @@ from redbot.core import commands
 from urllib.parse import quote
 
 log = logging.getLogger("red.zonk-cogs.opencritic")
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.WARN)
 
 class OpenCritic(commands.Cog):
     """Search for games' score on OpenCritic"""
@@ -19,41 +18,28 @@ class OpenCritic(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
-        self.session = aiohttp.ClientSession()
-
-    def cog_unload(self):
-        self.bot.loop.create_task(self.session.close())
 
     @commands.command()
     @commands.bot_has_permissions(embed_links=True, add_reactions=True)
     async def opencritic(self, ctx, *, title):
         """Search for a game"""
-        log.debug('in command now ' + str(datetime.now()))
 
-        # headers = {"accept": "application/json"}
-        session = self.session
-
+        await ctx.trigger_typing()
         # Queries api for a game
         searchUrl = API_URL + '/game/search?criteria=' + quote(title)
         data = []
 
-        log.debug('starting clientsession '+ str(datetime.now()))
-        # async with aiohttp.ClientSession() as session:
-        #     log.debug('starting get request '+ str(datetime.now()))
-        async with session.get(searchUrl) as response:
-            log.debug('checking for status '+ str(datetime.now()))
-            if response.status != 200:
-                response.raise_for_status()
-            log.debug('awaiting to json the response next '+ str(datetime.now()))
-            data = await response.json()
+        async with aiohttp.ClientSession() as session:
+            async with session.get(searchUrl) as response:
+                if response.status != 200:
+                    response.raise_for_status()
+                data = await response.json()
 
-        log.debug('received results from search term '+ str(datetime.now()))
         # Handle if nothing is found
         if len(data) == 0:
             await ctx.send("I couldn't find anything! Try it with another title!")
             return
 
-        # get array of gameIds
         gameIds = []
 
         for game in data:
