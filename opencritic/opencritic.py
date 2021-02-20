1a6805f70456c9786cbee3b38f6ae96674808acd
zawnk <zonkor@gmail.com>
2021-02-20 17:57:17 +0100

    ADd more logging

--- opencritic/opencritic.py
+++ opencritic/opencritic.py
@@ -32,13 +32,17 @@ class OpenCritic(commands.Cog):
         searchUrl = API_URL + '/game/search?criteria=' + quote(title)
         data = []
 
+        log.debug('starting clientsession '+ str(datetime.now()))
         async with aiohttp.ClientSession() as session:
+            log.debug('starting get request '+ str(datetime.now()))
             async with session.get(searchUrl) as response:
+                log.debug('checking for status '+ str(datetime.now()))
                 if response.status != 200:
                     response.raise_for_status()
+                log.debug('awaiting to json the response next '+ str(datetime.now()))
                 data = await response.json()
 
-        log.debug('received results from name '+ str(datetime.now()))
+        log.debug('received results from search term '+ str(datetime.now()))
         # Handle if nothing is found
         if len(data) == 0:
             await ctx.send("I couldn't find anything! Try it with another title!")
